a5c53b86bdc4df3c1db8512ceb7077b1f78062f1
Justin Herscu <jherscucode@gmail.com>
2022-06-11 15:50:26 -0700

    Make icon use case templates

---
+++ app/src/main/java/com/jHerscu/clearskies/domain/useCase/weather/icon/CacheIconUseCase.kt
@@ -0,0 +1,11 @@
+package com.jHerscu.clearskies.domain.useCase.weather.icon
+
+import javax.inject.Inject
+
+class CacheIconUseCase @Inject constructor(
+
+) {
+    suspend operator fun invoke() {
+
+    }
+}
