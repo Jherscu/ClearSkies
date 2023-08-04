b29994e98374871b6cc9c59f549098dc09dd32d7
Justin Herscu <jherscucode@gmail.com>
2023-08-03 21:40:12 -0500

    edit theme impl

--- app/src/main/java/com/jHerscu/clearskies/ui/MainActivity.kt
+++ app/src/main/java/com/jHerscu/clearskies/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.jHerscu.clearskies.ui
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,11 +18,20 @@ class MainActivity : ComponentActivity() {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
 
+        // Make app edge-to-edge
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        val windowInsetsController =
+            WindowCompat.getInsetsController(this.window, this.window.decorView)
+
+        windowInsetsController.isAppearanceLightNavigationBars =
+            resources.configuration.uiMode != UI_MODE_NIGHT_YES
+        // Set splash screen to obey delay set in its vm
         splashScreen.setKeepOnScreenCondition { splashViewModel.isVisible.value }
 
         setContent {
-            AppTheme {
+            AppTheme(
+                dynamicColor = true // TODO(jherscu): Add pref for user to toggle on/off Dynamic Theming in app
+            ) {
                 ClearSkiesApp()
             }
         }
